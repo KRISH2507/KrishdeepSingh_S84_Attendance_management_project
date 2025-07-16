948953053c358ae26428f94ec3a8a4d6e18f8ad6
KRISH2507 <thekrishdeepsingh@gmail.com>
2025-07-16 09:50:00 +0530

    part2

--- src/com/school/Main.java
+++ src/com/school/Main.java
@@ -2,7 +2,25 @@ package com.school;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to the School Attendance System Project!");
-        System.out.println("Session 1: Project Setup and Orientation Complete.");
+        Course c1 = new Course();
+        c1.setDetails(1 , "DBMS");
+        Course c2 = new Course();
+        c2.setDetails(2 , "LHT");
+
+
+        Student student = new Student();
+        student.setDetails(11 , "pratham");
+        Student student2 = new Student(); 
+        student2.setDetails(12 , "krish");
+        
+        Student[] arr2 = {student , student2};
+        System.out.println("Registered Students: ");
+        for(Student n : arr2) n.displayDetails();
+        Course [] arr1 = {c1 , c2};
+        System.out.println("Registered course: ");
+        for(Course c : arr1) c.displayDetails();
+        
+
+
     }
 }
